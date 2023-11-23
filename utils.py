eccf040da8dec3bd37834d243a0ce3622c7e47df
Pedro Henrique Santos Guimarães <pedro.guimaraes02@hotmail.com>
2023-11-22 21:01:10 -0300

    feat:tarefa 1 finalizada

---
+++ utils.py
@@ -0,0 +1,19 @@
+from datetime import datetime, timedelta
+from exceptions import *
+
+
+def data_processing(data):
+    if data["titles"] < 0:
+        raise NegativeTitlesError
+
+    first_cup_year = int(data["first_cup"][:4])
+    if first_cup_year < 1930 or (first_cup_year - 1930) % 4 != 0:
+        raise InvalidYearCupError
+
+    current_year = datetime.now().year
+    max_possible_titles = (current_year - first_cup_year) // 4 + 1
+
+    if data["titles"] > max_possible_titles:
+        raise ImpossibleTitlesError
+
+    return f"{data['name']} - Data Processed Successfully!"
